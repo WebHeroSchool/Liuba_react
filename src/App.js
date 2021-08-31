78743112c23174fffb59946c5ba993fe6448e51a
Liubovminakova <msnakova7@gmail.com>
2021-08-31 05:42:13 +0200

    operations added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const message = 'NUMBER'
+const number = 6
+const count = 2+10
+const result = true
+const age = 26;
+const beverage = (age >= 21) ? "Beer" : "Juice";
+let x;
+
+true || (x = 1)
+
+
+
 function App() {
   return (
     <div className="App">
@@ -9,9 +21,32 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p style={{
+          color: 'blue'
+        }}>
           Hello world!
         </p>
+        <p>
+          {}
+        </p>
+        <p>
+          {message}
+        </p>
+        <p>
+          {number}
+        </p>
+        <p>
+          {beverage}
+        </p>
+        <p>
+          {result && 'result is true'}
+        </p>
+        <p>
+          {x}
+        </p>
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
