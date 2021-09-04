336ae0393d25f45475c43b92e73f4c249390a9f3
Liubovminakova <msnakova7@gmail.com>
2021-09-04 11:42:31 +0200

    Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<h3>InputItem</h3>);
+const InputItem = () => (<div>
+  <TextField
+          id="standard-textarea"
+          label="Добавить задание"
+          fullWidth
+          margin="normal"
+          multiline
+        />
+</div>);
 
 export default InputItem;
