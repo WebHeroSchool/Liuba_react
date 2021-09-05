1ec145cd251c21ccbade34d1a63cd17f8bea7815
Liubovminakova <msnakova7@gmail.com>
2021-09-06 01:11:18 +0200

    State

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,30 +7,33 @@ import styles from './App.module.css';
 const todoItem ='Написать новое приложение';
 
 class App extends React.Component{
-  render(){
-    const items =[
-        {
-          value:'write my own styles',
-          isDone: true
-        },
-        {
-          value: 'finish 26 lesson',
-          isDone: true
-        },
-        {
-          value: 'start a new lesson',
-          isDone: false
-        }
-    ]
-  return(
-
-  <div className={styles.wrap}>
-      <h1 className={styles.title}>TO-DO LIST </h1>
-      <InputItem />
-      <ItemList items = {items} />
-      <Footer count ={3} />
-  </div>);
-  }
+  state ={
+    items: [
+      {
+        value:'write my own styles',
+        isDone: true
+      },
+      {
+        value: 'finish 26 lesson',
+        isDone: true
+      },
+      {
+        value: 'start a new lesson',
+        isDone: false
+      }
+  ]
 };
+
+render() {
+    return(
+      <div className={styles.wrap}>
+          <h1 className={styles.title}>TO-DO LIST </h1>
+          <InputItem />
+          <ItemList items = {this.state.items} />
+          <Footer count ={3} />
+      </div>);
+    }
+};
+
 
 export default App;
