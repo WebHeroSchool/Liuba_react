6800120a2ebdae4714538e020e46bc1feb0056b6
Liubovminakova <msnakova7@gmail.com>
2021-09-08 22:51:55 +0200

    something

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,7 +4,6 @@ import Checkbox from '@material-ui/core/Checkbox';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
-
 import styles from './Item.module.css';
 
 const Item = ({ item, classes, value, isDone, onClickDone, id }) =>(
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import Item from '../Item/Item'
 
-
  const ItemList = ({ items, onClickDone }) => (<List>
   {items.map(item => <li key={item.value}>
     <Item
