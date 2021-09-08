bafa26a4721ab55d0f5b87636aafded945046731
Liubovminakova <msnakova7@gmail.com>
2021-09-08 16:45:10 +0200

    did the corrections

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import classnames from 'classnames';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
 
 import styles from './Item.module.css';
 
-const Item = ({ value, isDone, onClickDone, id }) => (<span className={
-  classnames({
-    [styles.item]: true,
-    [styles.done]: isDone
-  })
-}>
-    {value}
-</span>);
+const Item = ({ item, classes, value, isDone, onClickDone, id }) =>(
+  <div>
+
+      <ListItem>
+          <Checkbox color="primary"
+          checked={isDone}
+          inputProps={{'aria-label': 'uncontrolled-checkbox'}}
+          onClick = {() => onClickDone(id)}/>
+        <ListItemText>
+             <span className={
+             classnames({
+               [styles.item]: true,
+               [styles.done]: isDone
+             })
+           }>
+               {value}
+           </span>)
+       </ListItemText>
+       <DeleteOutlineIcon />
+      </ListItem>
+</div>);
 
 export default Item;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,30 +1,17 @@
 import React from 'react';
- import Item from '../Item/Item'
- import Checkbox from '@material-ui/core/Checkbox';
- import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
- import ListItemText from '@material-ui/core/ListItemText';
- import ListItem from '@material-ui/core/ListItem';
- import ListItemIcon from '@material-ui/core/ListItemIcon';
+import List from '@material-ui/core/List';
+import Item from '../Item/Item'
 
- const ItemList = ({ items, isDone, onClickDone, id }) =>(<div>
 
-     {items.map(item => (
-       <ListItem key={item.value} className={item}>
-         <ListItemIcon>
-           <Checkbox color="primary"
-           inputProps={{'aria-label': 'uncontrolled-checkbox'}}
-           onClick = {() => onClickDone(item.id)}/>
-         </ListItemIcon>
-         <ListItemText>
-         <Item
-             value={item.value}
-             isDone={item.isDone}
-             id={item.id}
-             onClickDone={onClickDone}
-              />
-        </ListItemText>
-        <DeleteOutlineIcon />
-       </ListItem>
-     ))}
- </div>);
+ const ItemList = ({ items, onClickDone }) => (<List>
+  {items.map(item => <li key={item.value}>
+    <Item
+      value={item.value}
+      isDone={item.isDone}
+      id={item.id}
+      onClickDone = {onClickDone}
+    />
+  </li>)}
+</List>);
+
 export default ItemList;
