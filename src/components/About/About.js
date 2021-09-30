1101e3513640dd97922bd78ab48a0cb72f1eba4f
Liubovminakova <msnakova7@gmail.com>
2021-09-30 22:00:09 +0200

    add info about myself

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,85 @@
 import React from 'react';
 import styles from './About.module.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Octokit } from '@octokit/rest';
+import '../../images/background.png'
 
-const About = () => (
-  <div className={styles.wrap}>
-    <h1 className={styles.title}>About me:</h1>
-  </div>
-);
+const octokit = new Octokit();
+
+class About extends React.Component{
+  state = {
+    isLoading: true,
+    repoList: [],
+    infoProfile: [],
+    isError: false,
+    error: {}
+  }
+
+
+
+  componentDidMount () {
+          octokit.repos.listForUser({
+           username: 'Liubovminakova'
+    }).then(({ data }) => {
+      this.setState({
+        repoList: data,
+        isLoading: false
+      });
+    })
+
+    .catch(err => {
+      this.setState({
+        isLoading: false,
+        isError: true,
+        error: 'ERROR'
+      })
+    })
+
+
+
+
+  octokit.rest.users.getByUsername({
+  username: 'Liubovminakova'
+}).then(({ data }) => {
+  this.setState({
+    infoProfile: data,
+    isLoading: false
+  })
+})
+
+.catch(err =>{
+  this.setState({
+    isLoading: false,
+    isError: true,
+    error: 'ERROR'
+  })
+})
+
+
+
+}
+
+  render(){
+      const { isLoading, repoList, isError, error, infoProfile } = this.state;
+      return(
+          <div className={styles.wrap}>
+          { isLoading ? <CircularProgress /> :
+          <div>
+            <h1 className={styles.title}> {infoProfile.name} </h1>
+            <img className={styles.avatar} src={infoProfile.avatar_url} alt='Avatar'/>
+            <h3 className={styles.subtitle}> My repositories: </h3>
+            {isError ? 'Not found' + console.error:
+            <ol>
+            {repoList.map(repo =>(<li key={repo.id} className={styles.list}>
+              <a href={repo.svn_url} className={styles.element}> {repo.name} </a>
+              </li>))}
+            </ol>
+          }
+        </div>
+        }
+        </div>
+      );
+  }
+}
 
 export default About;
