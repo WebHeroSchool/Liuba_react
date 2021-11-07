876ff677a1f4f8fd0a43b00e348547d6c3b5d1cc
Liubovminakova <msnakova7@gmail.com>
2021-11-07 19:00:31 +0100

    Updates

--- src/components/Footer/Footer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Footer = ({ count }) => (<h3>Осталось выполныть вот столько дел: {count}</h3>);
-
-export default Footer;
--- src/components/InputItem/InputItem.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const InputItem = () => (<h3>InputItem</h3>);
-
-export default InputItem;
